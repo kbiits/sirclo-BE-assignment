a40b9f40665c89c6a6aeb334c64ece9c0ad15401
Nabiel Omar Syarif <nabilumarsyarif294@gmail.com>
2022-03-26 08:34:57 +0700

    add average row in index page

--- resources/views/berat.blade.php
+++ resources/views/berat.blade.php
@@ -41,15 +41,20 @@
 
     @php
         if ($type == 'index') {
-        $tableMap = ['Tanggal', 'Max', 'Min', 'Perbedaan', 'Aksi'];
+            $tableMap = ['Tanggal', 'Max', 'Min', 'Perbedaan', 'Aksi'];
         } else {
-        $tableMap = ['Tanggal', $dataBerat->date];
-        $tableData = [
-        'Max' => $dataBerat->max_weight,
-        'Min' => $dataBerat->min_weight,
-        'Perbedaan' => $dataBerat->max_weight - $dataBerat->min_weight,
-        ];
+            $tableMap = ['Tanggal', $dataBerat->date];
+            $tableData = [
+                'Max' => $dataBerat->max_weight,
+                'Min' => $dataBerat->min_weight,
+                'Perbedaan' => $dataBerat->max_weight - $dataBerat->min_weight,
+            ];
         }
+        $totalMax = 0;
+        $totalMin = 0;
+        $totalDiff = 0;
+        // I save this to variable for memoization, so we shouldn't compute it more than 1 times
+        $count = $dataBerat->count();
     @endphp
 
     <table class="styled-table">
@@ -61,11 +66,19 @@
         <tbody>
             @if($type == 'index')
                 @foreach($dataBerat as $row)
+                @php
+                    $maxWeight = $row->max_weight;
+                    $minWeight = $row->min_weight;
+                    $diff = $maxWeight - $minWeight;
+                    $totalMax += $maxWeight;
+                    $totalMin += $minWeight;
+                    $totalDiff += $diff;
+                @endphp
                     <tr onmouseover="setRowClass(event, true)" onmouseout="setRowClass(event, false)">
                         <td>{{ $row->date }}</td>
-                        <td>{{ $row->max_weight }}</td>
-                        <td>{{ $row->min_weight }}</td>
-                        <td>{{ $row->max_weight - $row->min_weight }}</td>
+                        <td>{{ $maxWeight }}</td>
+                        <td>{{ $minWeight }}</td>
+                        <td>{{ $diff }}</td>
                         <td>
                             <span>
                                 <a href="{{ route('berat.show', $row->id) }}">
@@ -95,6 +108,13 @@
                         </td>
                     </tr>
                 @endforeach
+                <tr style="font-weight: bold;">
+                    <td>Rata-rata</td>
+                    <td>{{ round($totalMax / $count, 2) }}</td>
+                    <td>{{ round($totalMin / $count, 2) }}</td>
+                    <td>{{ round($totalDiff / $count, 2) }}</td>
+                    <td></td>
+                </tr>
             @else
                 @foreach($tableData as $col => $val)
                     <tr>
